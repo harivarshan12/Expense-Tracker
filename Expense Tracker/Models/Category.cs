c1d3703d0f90d0733b73a304a5800c34cc4188de
Hari Varshan M <harivarshanmsd@gmail.com>
2024-06-02 23:03:15 +0530

    Added Transaction Controller

--- Expense Tracker/Models/Category.cs
+++ Expense Tracker/Models/Category.cs
@@ -8,6 +8,7 @@ public class Category
     [Key]
     public int CategoryId { get; set; }
     [Column(TypeName = "nvarchar(50)")]
+    [Required(ErrorMessage = "Title is required.")]
     public string Title { get; set; }
     [Column(TypeName = "nvarchar(5)")]
     public string Icon { get; set; }
@@ -18,7 +19,7 @@ public class Category
     {
         get
         {
-            return Title + " " + Icon;
+            return Icon + " " + Title;
         }
     }
 }
